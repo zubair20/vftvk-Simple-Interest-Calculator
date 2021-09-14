7fe1878097a1007e2a2e163b6c6590e2f8a2345c
zubair20 <zubair.akhtar20@gmail.com>
2021-09-14 07:25:28 +0000

    Fixes css and js bugs

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function compute()
 {
-    if(principal <=0){
+    
+	
+    var principal = document.getElementById("principal").value;
+	if(principal <=0){
         alert("Enter a positive number");
         return;
     }
-    var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result");
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <span class='highlight'\>"+principal+"\</span\>,\<br\>at an interest rate of <span class='highlight'\>"+rate+"%\</span\>\<br\>You will receive an amount of <span class='highlight'\>"+interest+"\</span\>,\<br\>in the year <span class='highlight'\>"+year+"\</span\>\<br\>"
 
 }
 
